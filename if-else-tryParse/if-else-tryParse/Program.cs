6686234ed72ad2ee8c274ad40bcc26d7783d7ee8
Brian <briantechdom@gmail.com>
2018-09-13 15:35:18 -0700

    Committing initial files

---
+++ if-else-tryParse/if-else-tryParse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_tryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the temperature like?");
+            string temperature = Console.ReadLine();
+            int numTemp = int.Parse(temperature);
+
+            if(numTemp < 20)
+            {
+                Console.WriteLine("Take the coat");
+            }
+
+            if(numTemp == 20)
+            {
+                Console.WriteLine("Pants and pull over should be fine");
+            }
+
+            if(numTemp > 20)
+            {
+                Consolee.WriteLine("Shorts are enough today");
+            }
+            Console.Read();
+        }
+    }
+}
